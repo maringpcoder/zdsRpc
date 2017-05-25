af77c7bda78d4ba3dda9c17d19de39f8d126c971
zhengdiao <289024114@qq.com>
2017-05-25 13:46:35 +0800

    异步回调器创建

---
+++ server/server.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: marin
+ * Date: 2017/5/25
+ * Time: 10:36
+ */
+ini_set('display_errors',"On");
+error_reporting(E_ALL|E_STRICT);
+//检查扩展是否安装
+if(!extension_loaded('yaf')){
+    exit('Please install yaf extension.'.PHP_EOL);
+}
+if(!extension_loaded('redis')){
+    exit('Please install redis extension.'.PHP_EOL);
+}
+if(!extension_loaded('swoole')){
+    exit('Please install swoole extension.'.PHP_EOL);
+}
+//该服务启动脚本必须在cli模式下，所以需要检查当前php运行模式
+if(php_sapi_name()!='cli'){
+    exit('Please use php cli model.'.PHP_EOL);
+}
+$cmd = '/usr/local/php/bin/php';
+function  syncServer()
+{
+    echo (yield ['mysqlpool']) .PHP_EOL;
+    echo (yield ['redispool']) .PHP_EOL;
+    echo (yield ['task']) .PHP_EOL;
+    echo (yield ['hprose']) .PHP_EOL;
+}
+
+//异步调用
+function asyncCaller(Generator $gen)
+{
+    global $cmd;
+    $task = $gen->current();
+    if(isset($task)){
+        switch ($task){
+            case 'mysqlpool':
+                foreach (glob(__DIR__.'/mysql/*.php') as $k =>$startUpFile){
+                    exec($cmd.' '.$startUpFile);
+                }
+                echo "Mysql Pool Service Success......".PHP_EOL;
+                $gen ->send("Mysql Pool Service Success!");
+                asyncCaller($gen);
+                break;
+            case 'redispool':
+                foreach (glob(__DIR__.'/cache/*.php') as $k => $startUpFile){
+                    exec($cmd.' '.$startUpFile);
+                }
+                echo "Cache Driver Pool Service Success......".PHP_EOL;
+                $gen->send("Cache Driver Pool Service Success!");
+                asyncCaller($gen);
+                break;
+            case 'task':
+                foreach(glob(__DIR__.'/swoole/Task*.php') as $startUpFile) {
+                    exec($cmd.' '.$startUpFile);
+                }
+                echo "Task Service start ....".PHP_EOL;//任务服务器服务
+                $gen->send('Task Service start OK!');
+                asyncCaller($gen);
+                break;
+            case 'hprose':
+                foreach (glob(__DIR__.'/') as $startUpFile){
+                    exec($cmd.' '.$startUpFile);
+                }
+                echo 'The Hprose Rpc Service Start......'.PHP_EOL;
+                $gen->send('The Hprose Rpc Service Start!');
+                asyncCaller($gen);
+                break;
+            default:
+                $gen ->send('no method');
+                break;
+        }
+    }
+}
+//argc??
+if($argc == 2){
+    $server_command = $argv;
+    !in_array($server_command[1],['start','stop','restart']) && exit('Argv is not Correct'.PHP_EOL);
+    switch ($server_command[1]){
+        case 'start'://启动服务
+            asyncCaller(syncServer());
+            break;
+        case 'stop'://停止服务
+            exec("ps -ef|grep -E '".$cmd."|grep -v 'grep'| awk'{print $2}'|xargs kill -9 > /dev/null 2>&1 &");
+            echo "Kill all process success.".PHP_EOL;
+            break;
+        case 'restart'://重启服务
+            exec("ps -ef|grep -E '".$cmd."|grep -v 'grep'| awk'{print $2}'|xargs kill -9 > /dev/null 2>&1 &");
+            echo "Kill all process success.".PHP_EOL;
+            asyncCaller(syncServer());
+            break;
+        default:
+            exit('This command is not support,try to use start|stop|restart'.PHP_EOL);
+            break;
+    }
+
+
+
+
+}else{
+    echo 'Please input Correct params example：'.PHP_EOL;
+    exit('php server.php start|stop|restart'.PHP_EOL);
+}
+
